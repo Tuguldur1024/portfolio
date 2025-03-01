770a6fb9a6eba1ca398d601d2dde09653e7e0806
Tuguldur1024 <purevochirbilguuntuguldur@gmail.com>
2025-03-01 12:08:08 +0800

    download cv done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,72 +2,66 @@ import NavigationLink from "./NavigationLink";
 import CvButton from "./CvButton";
 import LightIcon from "./icons/LightIcon";
 import InfoIcon from "./icons/InfoIcon";
-import Xmark from "./icons/Xmark";
 import NightIcon from "./icons/NightIcon";
 
-const links = [
-  { name: "About" },
-  { name: "Work" },
-  { name: "Testimonials" },
-  { name: "Contact" },
-];
-
-<div className="drawer">
-  <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-  <div className="drawer-content">
-    {/* Page content here */}
-    <label htmlFor="my-drawer" className="btn btn-primary drawer-button">
-      Open drawer
-    </label>
-  </div>
-  <div className="drawer-side">
-    <label
-      htmlFor="my-drawer"
-      aria-label="close sidebar"
-      className="drawer-overlay"
-    ></label>
-    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-      {/* Sidebar content here */}
-      <li>
-        <a>Sidebar Item 1</a>
-      </li>
-      <li>
-        <a>Sidebar Item 2</a>
-      </li>
-    </ul>
-  </div>
-</div>;
+const links = [{ name: "About" }];
+const cvFilePath = "/files/CV.pdf"; 
 
 const Navbar = () => {
   return (
-    <div class="flex items-center md:px-8 justify-between py-4 px-4">
-      <p class="font-normal text-start text-3xl font-rubikMono dark:text-gray-50	">
-        Tuguldur
-      </p>
-      <div class="lg:flex justify-between gap-4 items-center hidden">
-        <div class="flex justify-center gap-4">
-          {links.map((link, index) => {
-            return <NavigationLink key={index} text={link.name} />;
-          })}
+    <>
+      {/* Navbar Section */}
+      <div className="flex items-center md:px-8 justify-between py-4 px-4">
+        <p className="font-normal text-start text-3xl font-rubikMono dark:text-gray-50">
+          Tuguldur
+        </p>
+        <div className="lg:flex justify-between gap-4 items-center hidden">
+          <div className="flex justify-center gap-4">
+            {links.map((link, index) => (
+              <NavigationLink key={index} text={link.name} />
+            ))}
+          </div>
+          <div className="h-6 w-0 bg-gray-100 border"></div>
+          <div className="flex items-center gap-4">
+            <div onClick={toggleTheme}>
+              <LightIcon className="dark:hidden" />
+            </div>
+            <div onClick={toggleTheme}>
+              <NightIcon className="hidden dark:block" />
+            </div>
+            <a href={cvFilePath} download className="flex justify-center items-center bg-[#111827] rounded-lg text-white py-1.5 px-4 font-medium text-xl dark:bg-gray-50	 dark:text-gray-900">
+              Download CV
+            </a>
+          </div>
         </div>
-        <div class="h-6 w-0 bg-gray-100 border"></div>
-        <div class="flex items-center gap-4">
-          <div onClick={toggleTheme}>
-            <LightIcon className="dark:hidden" />
-          </div>
-          <div onClick={toggleTheme}>
-            <NightIcon className="hidden dark:block" />
-          </div>
-          <CvButton text="Download CV" />
+
+        <div className="lg:hidden">
+          <label htmlFor="my-drawer" className="drawer-button cursor-pointer">
+            <InfoIcon />
+          </label>
         </div>
       </div>
-      <div className="lg:hidden drawer-button">
-        <InfoIcon />
+
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content"></div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer" className="drawer-overlay"></label>
+          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+            <li>
+              <a href={cvFilePath} download className="flex justify-center items-center bg-[#111827] rounded-lg text-white py-1.5 px-4 font-medium text-xl dark:bg-gray-50	 dark:text-gray-900">
+                Download CV
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
+
 export default Navbar;
+
 const toggleTheme = () => {
   if (document.documentElement.classList.contains("dark")) {
     document.documentElement.classList.remove("dark");
